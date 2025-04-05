eda2ef94211998566b33abac55652bac712c944e
KieranGliver <kieran.glovjunk@gmail.com>
2025-04-05 00:57:58 -0400

    Added upload card function

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,15 +1,15 @@
 import { integer, pgTable, varchar, text } from "drizzle-orm/pg-core";
 export const cardsTable = pgTable("cards", {
   id: varchar({ length: 255 }).primaryKey().notNull(),
-  rarity: varchar({ length: 5 }).notNull(),
-  type: varchar({ length: 25 }).notNull(),
-  name: varchar({ length: 50 }).notNull(),
+  rarity: varchar({ length: 32 }).notNull(),
+  type: varchar({ length: 50 }).notNull(),
+  name: text().notNull(),
   cost: integer().notNull(),
-  attribute: varchar({ length: 25 }).notNull(),
+  attribute: varchar({ length: 50 }).notNull(),
   power: integer().notNull(),
   counter: integer().notNull(),
   colour: varchar({ length: 50 }).notNull(),
-  feature: varchar({ length: 50 }).notNull(),
+  feature: text().notNull(),
   set: varchar({ length: 50 }).notNull(),
   text: text().notNull(),
 });
